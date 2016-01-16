8bbb8f337ad651f20f398415e078191ea7d6fd8e
Peter Phan <peter.m.phan@gmail.com>
2016-01-15 16:49:39 -0800

    Complete admin page_show request spec

--- app/serializers/xing_cms_backend/admin/page_serializer.rb
+++ app/serializers/xing_cms_backend/admin/page_serializer.rb
@@ -1,11 +1,11 @@
-#require 'xing-backend'
-
 module XingCmsBackend
   class Admin::PageSerializer < PageSerializer
     attributes :url_slug, :published, :publish_start, :publish_end
 
     def links
-      { :self => engine_routes.admin_page_path(object), :public => engine_routes.page_path(object), :admin => engine_routes.admin_page_path(object) }
+      { :self => engine_routes.admin_page_path(:url_slug => object.url_slug),
+        :public => engine_routes.page_path(:url_slug => object.url_slug),
+        :admin => engine_routes.admin_page_path(:url_slug => object.url_slug) }
     end
 
     #TODO: Move this to where all engine serializers can use
--- spec/requests/xing_cms_backend/admin/page_show_spec.rb
+++ spec/requests/xing_cms_backend/admin/page_show_spec.rb
@@ -37,7 +37,7 @@ module XingCmsBackend
       )
     end
 
-    let :admin do FactoryGirl.create(:admin) end
+    #let :admin do FactoryGirl.create(:admin) end
 
     describe "GET admin/pages/:url_slug" do
       it "shows page as json" do
@@ -45,28 +45,29 @@ module XingCmsBackend
         json_get "/xing_cms_backend/admin/pages/#{page.url_slug}"
 
         expect(response).to be_success
-        #expect(response.body).to have_json_path("links")
-        #expect(response.body).to have_json_path("links/self")
-        #expect(response.body).to have_json_path("links/public")
-        #expect(response.body).to have_json_path("data")
-        #expect(response.body).to have_json_path("data/title")
-        #expect(response.body).to have_json_path("data/keywords")
-        #expect(response.body).to have_json_path("data/description")
-        #expect(response.body).to have_json_path("data/layout")
-        #expect(response.body).to have_json_path("data/contents")
-        #expect(response.body).to have_json_size(3).at_path("data/contents")
-        #expect(response.body).to be_json_eql("\"/admin/pages/#{page.url_slug}\"").at_path("links/self")
-        #expect(response.body).to be_json_eql("\"#{page.title}\"").at_path("data/title")
-        #expect(response.body).to be_json_eql("\"#{admin_content_block_path(headline)}\"").at_path("data/contents/headline/links/self")
-        #expect(response.body).to be_json_eql("\"#{styles.body}\"").at_path("data/contents/styles/data/body")
+        expect(response.body).to have_json_path("links")
+        expect(response.body).to have_json_path("links/self")
+        expect(response.body).to have_json_path("links/public")
+        expect(response.body).to have_json_path("data")
+        expect(response.body).to have_json_path("data/title")
+        expect(response.body).to have_json_path("data/keywords")
+        expect(response.body).to have_json_path("data/description")
+        expect(response.body).to have_json_path("data/layout")
+        expect(response.body).to have_json_path("data/contents")
+        expect(response.body).to have_json_size(3).at_path("data/contents")
+        expect(response.body).to be_json_eql("\"/xing_cms_backend/admin/pages/#{page.url_slug}\"").at_path("links/self")
+        expect(response.body).to be_json_eql("\"#{page.title}\"").at_path("data/title")
+        expect(response.body).to be_json_eql("\"#{engine_routes.admin_content_block_path(headline)}\"").at_path("data/contents/headline/links/self")
+        expect(response.body).to be_json_eql("\"#{styles.body}\"").at_path("data/contents/styles/data/body")
       end
     end
 
-    #describe "not authenticated" do
-      #it "should return not authorized" do
-        #json_put "admin/pages/#{page.url_slug}"
-        #expect(response.status).to be(401)
-      #end
-    #end
+    # TODO: Still need to bring in authentication
+    describe "not authenticated", :skip => true do
+      it "should return not authorized" do
+        json_put "admin/pages/#{page.url_slug}"
+        expect(response.status).to be(401)
+      end
+    end
   end
 end
