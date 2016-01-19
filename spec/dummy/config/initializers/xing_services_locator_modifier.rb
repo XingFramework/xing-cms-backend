cf67d59bbbd817613290cf4d144677be9bb4de64
Peter Phan <peter.m.phan@gmail.com>
2016-01-19 15:35:48 -0800

    Fix menu_item mapper specs

---
+++ spec/dummy/config/initializers/xing_services_locator_modifier.rb
@@ -0,0 +1,9 @@
+require 'xing-backend'
+
+#TODO: Is this how we want to accomplish setting engine routes instead of
+# application routes?
+Xing::Services::Locator.module_eval do
+  def router
+    XingCmsBackend::Engine.routes
+  end
+end
