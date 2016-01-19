890b89297dc3f15ae47bd98927479691cd2fa857
Peter Phan <peter.m.phan@gmail.com>
2016-01-19 15:58:00 -0800

    Start menu_item_update request spec

--- app/controllers/xing_cms_backend/admin/admin_controller.rb
+++ app/controllers/xing_cms_backend/admin/admin_controller.rb
@@ -2,6 +2,7 @@ module XingCmsBackend
   class Admin::AdminController < ApplicationController
     before_filter :reject_if_not_logged_in
 
+    #TODO: Need to figure out authorization
     def reject_if_not_logged_in
       # unless current_user
       #   render :json => {}, status: 401
